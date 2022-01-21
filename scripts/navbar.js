82cb0a3659a62c366edbde657b6592ac4c1b59ed
OybekKayumov <oybek.kayumov@gmail.com>
2022-01-21 21:27:49 +0500

    update navbar js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,32 @@
 const showList = document.querySelector('#list-link');
-const addnew = document.querySelector('#addlink');
+const addNew = document.querySelector('#addlink');
 const contactInfo = document.querySelector('#contact-link');
 const bookSection = document.querySelector('.container');
 const mainTitle = document.querySelector('#list-title');
 const addSection = document.querySelector('.add-section');
 const contact = document.querySelector('#contacts');
+
+function ListDisplay() {
+  bookSection.style.display = 'block';
+  mainTitle.style.display = 'block';
+  addSection.style.display = 'none';
+  contact.style.display = 'none';
+}
+
+function NewBook() {
+  addSection.style.display = 'block';
+  contact.style.display = 'none';
+  bookSection.style.display = 'none';
+  mainTitle.style.display = 'none';
+}
+
+function Getinfo() {
+  contact.style.display = 'block';
+  addSection.style.display = 'none';
+  bookSection.style.display = 'none';
+  mainTitle.style.display = 'none';
+}
+
+showList.addEventListener('click', ListDisplay);
+addNew.addEventListener('click', NewBook);
+contactInfo.addEventListener('click', Getinfo);
